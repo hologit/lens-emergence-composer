9cc2d8fa7ee3fcd3bbc9328f7fcdb06dc886d187
Chris Alfano <chris@jarv.us>
2019-10-31 03:37:04 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# lens-composer
+
+Process a `composer-requires/` tree and/or `composer.json` file to project PHP classes and bootstraps into an emergence runtime tree.
+
+## Debugging
+
+1. Create a build of the lens:
+
+    ```bash
+    HAB_ORIGIN=holo build /src/lens-composer
+    ```
+
+2. Symlink built package to working tree sources:
+
+    ```bash
+    ln -sf /src/lens-composer/lens-tree $(hab pkg path holo/lens-composer)/bin/lens-tree
+    rm -rf $(hab pkg path holo/lens-composer)/node_modules
+    ln -sf /src/lens-composer/node_modules $(hab pkg path holo/lens-composer)/node_modules
+    ```
+
+3. Edit shebang line in [`lens-tree`](./lens-tree) to enable node debugger:
+
+    ```bash
+    #!/bin/node --inspect-brk=0.0.0.0
+    ```
+
+4. Launch `attach to lens-composer` debugger task
+5. Execute projection on a holobranch using the lens within a studio:
+
+    ```bash
+    git holo project emergence-site --working --debug
+    ```
---
+++ plan.sh
@@ -0,0 +1,57 @@
+pkg_name=lens-composer
+pkg_origin=holo
+pkg_version="0.1.0"
+pkg_maintainer="Chris Alfano <chris@jarv.us>"
+pkg_license=("MIT")
+pkg_build_deps=(
+)
+pkg_deps=(
+  core/composer
+  core/git
+  core/node
+)
+pkg_bin_dirs=(bin)
+
+
+do_setup_environment() {
+  set_buildtime_env COMPOSER_ALLOW_SUPERUSER "1"
+}
+
+do_build() {
+  # build_line "Copying core (excluding vendor)"
+  # pushd "${PLAN_CONTEXT}" > /dev/null
+  # find . \
+  #   -maxdepth 1 -mindepth 1 \
+  #   -not -name 'plan.sh' \
+  #   -not -name 'vendor' \
+  #   -not -name '.git*' \
+  #   -exec cp -r '{}' "${CACHE_PATH}/{}" \;
+  # popd > /dev/null
+
+  pushd "${CACHE_PATH}" > /dev/null
+
+  build_line "Generating lens script"
+  mkdir -v "bin"
+  cp -v "${PLAN_CONTEXT}/lens-tree" ./bin/
+  fix_interpreter "bin/*" core/node bin/node
+
+  build_line "Running: npm install"
+  cp -v "${PLAN_CONTEXT}"/package{,-lock}.json ./
+  npm install
+
+  build_line "Running: composer require wikimedia/composer-merge-plugin"
+  composer require wikimedia/composer-merge-plugin \
+    --no-interaction \
+    --optimize-autoloader \
+    --classmap-authoritative
+
+  popd > /dev/null
+}
+
+do_install() {
+  cp -r "${CACHE_PATH}"/* "${pkg_prefix}/"
+}
+
+do_strip() {
+  return 0
+}
